e13801faa491014c2e4a0bd97e56828ee1a9d48a
erkezhanergeshbai <nokano0@mail.ru>
2026-02-01 21:33:15 +0600

    feat(generics): implement generic CrudRepository<T>

---
+++ src/repository/CrudRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import java.util.List;
+
+public interface CrudRepository<T> {
+    T save(T entity);
+    T findById(int id);
+    List<T> findAll();
+    void deleteById(int id);
+}
