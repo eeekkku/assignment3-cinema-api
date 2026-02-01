f6817f8a3b1b3b8115440d748159b2cb5812a759
erkezhanergeshbai <nokano0@mail.ru>
2026-02-01 21:32:50 +0600

    feat(interface): add Validatable interface with default/static methods

--- src/model/Validatable.java
+++ src/model/Validatable.java
@@ -1,5 +1,16 @@
 package model;
 
-public interface Validatable {
-    void validate() throws IllegalArgumentException;
+public interface Validatable<T> {
+
+    void validate();
+
+    default boolean isNull(T value) {
+        return value == null;
+    }
+
+    static void require(boolean condition, String message) {
+        if (!condition) {
+            throw new IllegalArgumentException(message);
+        }
+    }
 }
